513c24027a8ddb98a4cacdfd7e99bf412ab2f1a7
Daniel-Freitas0 <daniel.siqueira@sptech.school>
2023-03-03 18:01:53 -0300

    limpando react e preparando para o projeto

---
+++ src/componentes/Banner/Index.js
@@ -0,0 +1,13 @@
+import './Banner.css'
+
+const Banner = () => {
+    //JSX
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="banner principal" />
+        </header>
+
+    )
+}
+
+export default Banner;
