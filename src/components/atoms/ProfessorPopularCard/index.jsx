81fdefbab9091a20f63cb93ecd560d321954e21f
JoaoVictorHengler <joao.hengler@sptech.school>
2023-05-28 04:17:36 -0300

    [REFACTOR] - Modificando css da página encontrar professor

---
+++ src/components/atoms/ProfessorPopularCard/index.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Box, Avatar, Typography } from "@mui/material";
+import { deepOrange } from '@mui/material/colors';
+import GradeRoundedIcon from '@mui/icons-material/GradeRounded';
+import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
+import "./style.css";
+
+function ProfessorPopularCard(props) {
+  return (
+    <Box className="professor-popular-card">
+
+      <Box className="card-info-usuario">
+        <Avatar id="avatar-usuario-botao" className="avatar-usuario-img"
+          sx={{ bgcolor: deepOrange[500] }}>{props.nome[0]}</Avatar>
+        <Typography className="card-info-nome">{props.nome}</Typography>
+      </Box>
+
+      <Box className="card-info-estrela">
+        <GradeRoundedIcon  className="card-info-estrela-icon"/>
+        <Typography className="card-info-estrela-valor">{props.avaliacao}</Typography>
+        <FiberManualRecordIcon className="card-info-separacao" />
+        <Typography className="card-info-estrela-cidade" variant="subtitle2">{props.localizacao}</Typography>
+      </Box>
+
+      <Box className="card-info-status">
+        <FiberManualRecordIcon className="card-info-separacao" />
+        <Typography className="card-info-status-hora" variant="subtitle2">{props.status}</Typography>
+      </Box>
+    </Box>
+  );
+}
+
+export default ProfessorPopularCard;
--- src/components/templates/EncontrarProfessorConteudo/index.jsx
+++ src/components/templates/EncontrarProfessorConteudo/index.jsx
@@ -1,18 +1,35 @@
 import React from "react";
 import {
-    Box
+    Box,
+    Typography,
 } from "@mui/material";
-import ProfessorEmAlta from "../../molecules/ProfessoresAlta/index.jsx";
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import SearchIcon from '@mui/icons-material/Search';
+import ProfessoresPopulares from "../../molecules/ProfessoresPopulares/index.jsx";
 import Mapa from "../../atoms/Mapa/index.jsx";
 import "./style.css"
 
 function EncontrarProfessorConteudo(props) {
     return (
         
-        <>
-        <ProfessorEmAlta/>
-        <Mapa/>
-        </>
+        <Box className="encontrar-professor-container">
+            <Box className="professores-busca-card">
+                <Box className="professores-busca-lugar">
+                    <LocationOnIcon/>
+                    <Typography variant="subtitle1" className="professores-busca-lugar-nome">
+                        São Paulo
+                    </Typography>
+                </Box>
+                <Box className="professores-busca-campo">
+                    <Typography variant="subtitle1" className="professores-busca-campo-titulo">
+                        Buscar
+                    </Typography>
+                    <SearchIcon/>
+                </Box>
+            </Box>
+            <ProfessoresPopulares/>
+            <Mapa/>
+        </Box>
     );
 }
 
