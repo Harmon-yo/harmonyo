facb35b20a52733b5fa60dcb87e99f950f5d01d6
Vinicius-Alves02 <vinicius.vasques@sptech.school>
2023-06-06 14:20:14 -0300

    [REFACTOR] - Refatorada a section Como Funciona da página home

--- src/components/organisms/ComoFunciona/index.jsx
+++ src/components/organisms/ComoFunciona/index.jsx
@@ -1,19 +1,101 @@
 import React from "react";
 import BoxWithContainer from "../../atoms/BoxWithContainer";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Tabs, Tab } from "@mui/material";
+import wave from "../../../imgs/wave-como-funciona.png"
+import Card from "../../atoms/Card";
+import UserIcon from "../../../imgs/Icon-usuario-como-funciona.png"
 import "./style.css"
 
 function ComoFunciona(props) {
+
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (evento, novoValor) => {
+        setValue(novoValor)
+    };
+
+    function getTabProps(index) {
+        return {
+            id: `simple-tab-${index}`,
+            className: "dashboard-professor-tabs-item",
+            'aria-controls': `simple-tabpanel-${index}`,
+        };
+    }
+
     return (
-        <section>
+        <section className="section-como-funciona">
             <BoxWithContainer
                 boxClassName="como-funciona-box"
                 containerClassName="como-funciona-container">
-                    <Typography className="title como-funciona-titulo">Como funciona</Typography>
-                    <Box className="como-funciona-img">
+                    <Typography className="title como-funciona-titulo">Como funciona</Typography>                   
+
+                    <Box className="como-funciona-cards">
+
+                    <Tabs value={value} onChange={handleChange} aria-label="tabs" className="dashboard-professor-tabs">
+                        <Tab label="Aluno" {...getTabProps(0)} className="tabs-como-funciona-label"/>
+                        <Tab label="Professor" {...getTabProps(1)} className="tabs-como-funciona-label"/>
+                    </Tabs>
+
+                    <Box className= "box-cards">
+
+                        <Card className="card-como-funciona">
+                            <img src={UserIcon} alt="" className="icon-usuario"/>
+                            <Box>
+                            <Typography className="card-titulo">
+                                Cadastro           
+                            </Typography>
+
+                            <Typography className="card-texto">
+                                Realize seu cadastro como aluno
+                            </Typography>
+                            </Box>
+                        </Card>
+
+                        <Card className="card-como-funciona">
+                            <img src={UserIcon} alt="" className="icon-usuario"/>
+                            <Box>
+                            <Typography className="card-titulo">
+                                Cadastro           
+                            </Typography>
+
+                            <Typography className="card-texto">
+                                Realize seu cadastro como aluno
+                            </Typography>
+                            </Box>
+                        </Card>
+
+                        <Card className="card-como-funciona">
+                            <img src={UserIcon} alt="" className="icon-usuario"/>
+                            <Box>
+                            <Typography className="card-titulo">
+                                Cadastro           
+                            </Typography>
+
+                            <Typography className="card-texto">
+                                Realize seu cadastro como aluno
+                            </Typography>
+                            </Box>
+                        </Card>
+
+                        <Card className="card-como-funciona">
+                            <img src={UserIcon} alt="" className="icon-usuario"/>
+                            <Box>
+                            <Typography className="card-titulo">
+                                Cadastro           
+                            </Typography>
+
+                            <Typography className="card-texto">
+                                Realize seu cadastro como aluno
+                            </Typography>
+                            </Box>
+                        </Card>
 
                     </Box>
+                </Box>
+                    
+                    
             </BoxWithContainer>
+            <img src={wave} className="wave-como-funciona-img" />
         </section>
     );
 }
