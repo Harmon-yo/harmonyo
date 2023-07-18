dff3d3028e48700cc501ab0ab821262baf27d2d6
JoaoVictorHengler <joao.hengler@sptech.school>
2023-07-18 18:01:59 -0300

    [FIX] - Conflito entre páginas arrumado

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,9 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import VLibras from "@djpfs/react-vlibras";
-import ListarPedidos from "./pages/Global/ListarPedidos";
 const Home = React.lazy(() => import("./pages/Home"));
 const Login = React.lazy(() => import("./pages/Cadastro-Login/Login/Login"));
+const ListarPedidos = React.lazy(() => import("./pages/Global/ListarPedidos"));
 const Cadastro = React.lazy(() =>
   import("./pages/Cadastro-Login/Cadastro/Cadastro")
 );
