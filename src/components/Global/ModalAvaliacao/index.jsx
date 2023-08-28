d63372719d3041549be7e9df4af873fa2bdd896c
VictorRubinec <victor.rubinec@sptech.school>
2023-08-28 19:43:41 -0300

    [REFACTOR] - ajustando o front-end do modal de avaliacao / [FEATURE] - Rating mudando a quantidade de estrelas quando clica

--- src/components/Global/ModalAvaliacao/index.jsx
+++ src/components/Global/ModalAvaliacao/index.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import "./style.css";
-import { Box, Avatar, Rating, Button, Modal, Typography, TextField} from "@mui/material";
+import { Box, Avatar, Rating, Button, Modal, Typography, TextField } from "@mui/material";
+import CloseIcon from '@mui/icons-material/Close';
 
 function ModalAvaliacao(props) {
 
     const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+    const [value, setValue] = React.useState(0);
 
     return (
-        <Box>
-            <Button onClick={handleOpen}>Open modal</Button>
+        <Box className="modal">
+            <Button className="modal-button-open" onClick={handleOpen}>Avaliar Aula</Button>
             <Modal
                 open={open}
                 onClose={handleClose}
@@ -18,15 +20,36 @@ function ModalAvaliacao(props) {
                 aria-describedby="modal-modal-description"
             >
                 <Box className="modal-avaliacao">
-                    <Avatar sx={{ width: 100, height: 100 }}></Avatar><Typography id="modal-modal-title" variant="h8" component="h2">
-                        André Silva
-                    </Typography>
-                    <Rating value={5} readOnly></Rating>
-                    <Box className="avaliacao-numero">0.0</Box>
-                    
-                    <TextField id="outlined-basic" variant="outlined" />
-
-                    <Button variant="contained">Avaliar</Button>
+                    <Box className="modal-area">
+                        <Box className="modal-head">
+                            <Avatar sx={{ width: 90, height: 90 }}></Avatar>
+                            <Box className="modal-usuario">
+                                <Typography id="modal-modal-title" variant="h8" component="h2">Nome</Typography>
+                                <Box className="avaliacao-usuario">
+                                    <Rating
+                                        value={value}
+                                        precision={0.5}
+                                        size="medium"
+                                        onChange={(event, newValue) => {
+                                            setValue(newValue);
+                                        }}>
+                                    </Rating>
+                                    <Box className="avaliacao-numero">{value}</Box>
+                                </Box>
+                            </Box>
+                        </Box>
+                        <Box className="modal-close">
+                            <Button className="button-close" onClick={handleClose}>
+                                <CloseIcon />
+                            </Button>
+                        </Box>
+                    </Box>
+                    <Box className="modal-body">
+                        <Box className="box-text-input">
+                            <TextField className="modal-text-input" autoFocus multiline rows={6} />
+                        </Box>
+                        <Button className="modal-button">Avaliar</Button>
+                    </Box>
                 </Box>
             </Modal>
         </Box>
