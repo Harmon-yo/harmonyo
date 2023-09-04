e2db3151997267c9f784cee4352fe4c5bd5b9f4c
VictorRubinec <victor.rubinec@sptech.school>
2023-09-03 21:44:28 -0300

    [FEATURE] - back-end modal avaliacao

--- src/components/Global/ListarPedidos/Tabela/index.jsx
+++ src/components/Global/ListarPedidos/Tabela/index.jsx
@@ -1,13 +1,12 @@
 import {
   Button,
-  Paper,
+  Box,
   Table,
   TableBody,
   TableCell,
   TableContainer,
   TableHead,
   TableRow,
-  Typography,
 } from "@mui/material";
 import { useState, useEffect } from "react";
 import "./style.css";
@@ -15,27 +14,29 @@ import PerfilUsuario from "../PerfilUsuario";
 import Status from "../Status";
 import ModalDetalhes from "../ModalDetalhes";
 import api from "../../../../api";
+import ModalAvaliacao from "../../ModalAvaliacao";
+
 function Tabela() {
   const [dadosPedidos, setDadosPedidos] = useState([]);
 
   useEffect(() => {
-    api.get(`/pedidos/usuario/${sessionStorage.ID}`, 
-    { headers: { Authorization: `Bearer ${sessionStorage.TOKEN}` } }).then((response) => {
-      let pedidos = response.data;
-      pedidos = pedidos.sort(
-        (a, b) => {
-          return new Date(b.horaCriacao) - new Date(a.horaCriacao);
-        }
-      )
-      setDadosPedidos(response.data);
-      console.log(response.data);
-    });
+    api.get(`/pedidos/usuario/${sessionStorage.ID}`,
+      { headers: { Authorization: `Bearer ${sessionStorage.TOKEN}` } }).then((response) => {
+        let pedidos = response.data;
+        pedidos = pedidos.sort(
+          (a, b) => {
+            return new Date(b.horaCriacao) - new Date(a.horaCriacao);
+          }
+        )
+        setDadosPedidos(response.data);
+        console.log(response.data);
+      });
 
   }, [])
 
   const [open, setOpen] = useState(false);
   const [pedido, setPedido] = useState();
-  
+
   const handleOpen = (ped) => {
     console.log(ped);
     setOpen(true);
@@ -50,43 +51,47 @@ function Tabela() {
   }
 
   return (
-    <TableContainer style={{maxHeight: "90%", overflow: "scroll"}}>
+    <TableContainer style={{ maxHeight: "90%", overflow: "scroll" }}>
       <Table stickyHeader sx={{ minWidth: 650 }} aria-label="simple table" >
         <TableHead className="head">
           <TableRow>
             <TableCell className="topCelula" align="left">
               {sessionStorage.getItem("CATEGORIA") == "Professor" ? "Aluno" : "Professor"}
             </TableCell>
-            <TableCell  className="topCelula" align="center">Instrumento</TableCell>
+            <TableCell className="topCelula" align="center">Instrumento</TableCell>
             <TableCell className="topCelula" align="center">Status</TableCell>
             <TableCell className="topCelula" align="center">Data</TableCell>
-            <TableCell className="topCelula"  align="center">Valor</TableCell>
-            <TableCell className="topCelula"  align="center"></TableCell>
+            <TableCell className="topCelula" align="center">Valor</TableCell>
+            <TableCell className="topCelula" align="center"></TableCell>
 
           </TableRow>
         </TableHead>
-        <TableBody className="conteudoTabela"> 
+        <TableBody className="conteudoTabela">
           {dadosPedidos.map((row) => (
-                <TableRow
-                key={row.id}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                <TableCell  className="bodyCelula" align="left">
-                <PerfilUsuario nome={sessionStorage.CATEGORIA == "Professor"? row.aluno.nome : row.professor.nome}/>
-                </TableCell>
-                <TableCell className="bodyCelula" align="center">{row.aula.instrumento.nome}</TableCell>
-                <TableCell className="bodyCelula"  align="center"><Status status={row.status.descricao}/></TableCell>
-                <TableCell className="bodyCelula"  align="center">{handleData(row.dataAula)}</TableCell>
-                <TableCell className="bodyCelula" style={{fontWeight: "bold"}}  align="left">R$ {(row.aula.valorAula).toFixed(2)}</TableCell>
-                <TableCell className="bodyCelula"  align="center">
-                    <Button variant="outlined" className="botao" onClick={()=>{handleOpen(row)}}> Detalhes</Button>
-                </TableCell>
-                </TableRow>
+            <TableRow
+              key={row.id}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell className="bodyCelula" align="left">
+                <PerfilUsuario nome={sessionStorage.CATEGORIA == "Professor" ? row.aluno.nome : row.professor.nome} />
+              </TableCell>
+              <TableCell className="bodyCelula" align="center">{row.aula.instrumento.nome}</TableCell>
+              <TableCell className="bodyCelula" align="center"><Status status={row.status.descricao} /></TableCell>
+              <TableCell className="bodyCelula" align="center">{handleData(row.dataAula)}</TableCell>
+              <TableCell className="bodyCelula" style={{ fontWeight: "bold" }} align="left">R$ {(row.aula.valorAula).toFixed(2)}</TableCell>
+              <TableCell className="bodyCelula" align="center">
+                {row.status.descricao === "Concluído" ?
+                  <ModalAvaliacao stateUsuario={row}></ModalAvaliacao>
+                  :
+                  <Button variant="outlined" className="botao" onClick={() => { handleOpen(row) }}> Detalhes</Button>
+                }
+              </TableCell>
+            </TableRow>
           ))}
         </TableBody>
       </Table>
       <ModalDetalhes open={open} onClose={handleClose} pedido={pedido} />
-    </TableContainer>
+    </TableContainer >
   );
 }
 
--- src/components/Global/ModalAvaliacao/index.jsx
+++ src/components/Global/ModalAvaliacao/index.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 import { Box, Avatar, Rating, Button, Modal, Typography, TextField } from "@mui/material";
 import CloseIcon from '@mui/icons-material/Close';
+import { storage } from "../../../utils/firebase";
+import api from "../../../api";
 
 function ModalAvaliacao(props) {
 
@@ -10,50 +12,159 @@ function ModalAvaliacao(props) {
     const handleClose = () => setOpen(false);
     const [value, setValue] = React.useState(0);
 
-    return (
-        <Box className="modal">
-            <Button className="modal-button-open" onClick={handleOpen}>Avaliar Aula</Button>
-            <Modal
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
-            >
-                <Box className="modal-avaliacao">
-                    <Box className="modal-area">
-                        <Box className="modal-head">
-                            <Avatar sx={{ width: 90, height: 90 }}></Avatar>
-                            <Box className="modal-usuario">
-                                <Typography id="modal-modal-title" variant="h8" component="h2">Nome</Typography>
-                                <Box className="avaliacao-usuario">
-                                    <Rating
-                                        value={value}
-                                        precision={0.5}
-                                        size="medium"
-                                        onChange={(event, newValue) => {
-                                            setValue(newValue);
-                                        }}>
-                                    </Rating>
-                                    <Box className="avaliacao-numero">{value}</Box>
+    const [imgPerfilURL, setImgPerfilURL] = useState("")
+
+    const [formEnviar, setFormEnviar] = useState({
+        dadosEnviar: false
+    })
+
+    const [comentario, setComentario] = useState("")
+
+    const [pedidoAvaliado, setPedidoAvaliado] = useState(false);
+
+    api.get(`/usuarios/${props.stateUsuario.id}/${sessionStorage.ID}`,
+        { headers: { Authorization: `Bearer ${sessionStorage.TOKEN}` } }).then((response) => {
+            setPedidoAvaliado(response.data);
+        });
+
+    // Obtendo a URL da imagem de perfil
+    function obterImgPerfil() {
+
+        let urlImg = "";
+
+        if (sessionStorage.CATEGORIA === "Professor") {
+            urlImg = `/imgs-perfil-usuario/${props.stateUsuario.aluno.id}_ft_perfil`;
+        } else {
+            urlImg = `/imgs-perfil-usuario/${props.stateUsuario.professor.id}_ft_perfil`;
+        }
+
+
+        storage.ref(urlImg).getDownloadURL()
+            .then(url => {
+                console.log(url)
+                setImgPerfilURL(url)
+            })
+    }
+
+    function validacaoDados() {
+        if (value === 0) {
+            alert("Por favor, avalie o usuário!")
+            return false
+        }
+        if (comentario === "") {
+            alert("Por favor, escreva um comentário!")
+            return false
+        }
+        return true
+    }
+
+    function enviarAvaliacao() {
+
+        let avaliacao = {
+            usuarioAvaliadorId: parseInt(sessionStorage.ID),
+            valor: value,
+            comentario: comentario,
+            pedidoId: props.stateUsuario.aula.id,
+        }
+
+        console.log(avaliacao)
+
+        let idAvaliado = 0;
+
+        sessionStorage.CATEGORIA === "Professor" ?
+            idAvaliado = props.stateUsuario.aluno.id :
+            idAvaliado = props.stateUsuario.professor.id
+
+
+        api.post(`/usuarios/${idAvaliado}/avaliacoes`, avaliacao, { headers: { Authorization: `Bearer ${sessionStorage.TOKEN}` } })
+            .then(res => {
+                alert("Avaliação felita com sucesso!")
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+
+
+    // Usar esse modelo abaixo:
+    // <ModalAvaliacao stateUsuario={{informacoesAvaliado, setInformacoesAvaliado}}></ModalAvaliacao>
+
+    if (!pedidoAvaliado) {
+        return (
+            <Box className="modal">
+                <Button className="modal-button-open" onClick={handleOpen}>Avaliar Aula</Button>
+                <Modal
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                >
+                    <Box className="modal-avaliacao">
+                        <Box className="modal-area">
+                            <Box className="modal-head">
+                                <Avatar
+                                    sx={{ width: 90, height: 90, backgroundColor: '#099250', fontSize: 35 }}
+                                    src={imgPerfilURL}>
+                                    {sessionStorage.CATEGORIA === "Professor" ?
+                                        props.stateUsuario.aluno.nome.charAt(0) :
+                                        props.stateUsuario.professor.nome.charAt(0)}
+                                </Avatar>
+                                <Box className="modal-usuario">
+                                    <Typography id="modal-modal-title" variant="h8" component="h2">
+                                        {sessionStorage.CATEGORIA === "Professor" ?
+                                            props.stateUsuario.aluno.nome :
+                                            props.stateUsuario.professor.nome}
+                                    </Typography>
+                                    <Box className="avaliacao-usuario">
+                                        <Rating
+                                            className="valor_avaliacao"
+                                            value={value}
+                                            precision={1}
+                                            size="medium"
+                                            onChange={(event, newValue) => {
+                                                setValue(newValue);
+                                            }}>
+                                        </Rating>
+                                        <Box className="avaliacao-numero">{value}</Box>
+                                    </Box>
                                 </Box>
                             </Box>
+                            <Box className="modal-close">
+                                <Button className="button-close" onClick={handleClose}>
+                                    <CloseIcon />
+                                </Button>
+                            </Box>
                         </Box>
-                        <Box className="modal-close">
-                            <Button className="button-close" onClick={handleClose}>
-                                <CloseIcon />
-                            </Button>
+                        <Box className="modal-body">
+                            <Box className="box-text-input">
+                                <TextField className="comentario_avaliacao"
+                                    onChange={(e) => setComentario(e.target.value)}
+                                    autoFocus multiline rows={6} />
+                            </Box>
+                            <Button
+                                onClick={() => {
+                                    if (validacaoDados()) {
+                                        setFormEnviar({ ...formEnviar, dadosEnviar: enviarAvaliacao() })
+                                    }
+                                }}
+                                className="modal-button">Avaliar</Button>
                         </Box>
                     </Box>
-                    <Box className="modal-body">
-                        <Box className="box-text-input">
-                            <TextField className="modal-text-input" autoFocus multiline rows={6} />
-                        </Box>
-                        <Button className="modal-button">Avaliar</Button>
+                </Modal>
+            </Box>
+        );
+    } else {
+        return (
+            <>
+                <Box className="modal">
+                    <Box className="avaliado">
+                        <Box className="box-avaliado">Avaliado</Box>
                     </Box>
                 </Box>
-            </Modal>
-        </Box>
-    );
+            </>
+        );
+    }
 }
 
 export default ModalAvaliacao;
