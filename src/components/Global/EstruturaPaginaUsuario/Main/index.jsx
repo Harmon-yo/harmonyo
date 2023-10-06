439724c7e13381ddc3e823b4a27c9fd58f724196
JoaoVictorHengler <joao.hengler@sptech.school>
2023-10-05 21:38:16 -0300

    [FIX | RELEASE] - Arrumando problema no conteúdo ao realizar o pedido | Método colocado para carregar toda página em vez de carregar por partes

--- src/components/Global/EstruturaPaginaUsuario/Main/index.jsx
+++ src/components/Global/EstruturaPaginaUsuario/Main/index.jsx
@@ -2,22 +2,59 @@ import { useState, useEffect } from "react";
 import NavbarLateral from "../NavbarLateral/Main/index.jsx";
 import ContainerUsuario from "../ContainerUsuario/index.jsx";
 import { Box, Alert, IconButton } from "@mui/material";
+import Loading from "../../../../pages/Global/Loading/index.jsx";
 
 import { Close as CloseIcon } from "@mui/icons-material";
 import "./style.css";
 
 /* =================== Navbar ======================== */
 
+function selectAviso(aviso, setAvisosState, avisos, index) {
+  let ComponenteAviso;
+
+  switch (aviso.tipo) {
+    case "erro":
+      ComponenteAviso = Alert;
+      break;
+  }
+
+  return (<ComponenteAviso
+    key={index}
+    severity="error"
+    action={
+      <IconButton
+        aria-label="close"
+        color="inherit"
+        size="small"
+        onClick={() => setAvisosState(avisos.filter((_, i) => i !== index))}
+      >
+        <CloseIcon fontSize="inherit" />
+      </IconButton>
+    }
+    sx={{
+      zIndex: 1000,
+    }}
+  >
+    {aviso.mensagem}
+  </ComponenteAviso>)
+}
+
 function EstruturaPaginaUsuario(props) {
+  const [avisos, setAvisos] = useState([]);
+  const avisosState = props.avisosState;
+
   const [recarregarImgPagina, setRecarregarImgPagina] = useState(false);
-  const [erros, setErros] = useState([]);
   const recarregarTodasImgs = props.recarregarTodasImgs;
 
+
+  const [carregarPagina, setCarregarPagina] = useState(false);
+  const carregarPaginaForcado = props.carregarPaginaForcado;
+
   useEffect(() => {
-    if (props.errosState) {
-      setErros(props.errosState.erros);
+    if (avisosState) {
+      setAvisos(avisosState.avisos);
     }
-  }, [props.errosState]);
+  }, [avisosState]);
 
   useEffect(() => {
     if (recarregarTodasImgs) {
@@ -25,40 +62,27 @@ function EstruturaPaginaUsuario(props) {
     }
   }, [recarregarTodasImgs]);
 
+  useEffect(() => {
+    if (carregarPaginaForcado) {
+      setCarregarPagina(carregarPaginaForcado);
+    } else {
+      setCarregarPagina(true);
+    }
+  }, [carregarPaginaForcado]);
+
 
   return (
     <Box className="mui-root">
       <Box className="erro-container">
-        {erros &&
-          erros.map((erro, index) => {
-            return (
-              <Alert
-                key={index}
-                severity="error"
-                action={
-                  <IconButton
-                    aria-label="close"
-                    color="inherit"
-                    size="small"
-                    onClick={() => props.errosState.setErros(erros.filter((_, i) => i !== index))}
-                  >
-                    <CloseIcon fontSize="inherit" />
-                  </IconButton>
-                }
-                sx={{
-                  zIndex: 1000,
-                }}
-              >
-                {erro}
-              </Alert>
-            );
+        {avisos &&
+          avisos.map((erro, index) => {
+            return selectAviso(erro, avisosState.setAvisos, avisosState.avisos, index);
           })}
       </Box>
       <NavbarLateral tela={props.tela} />
-      {/* <h1>Olá {sessionStorage.CATEGORIA}(a) {sessionStorage.NOME} - Encontrar Professores</h1> */}
       <Box className="pagina-padrao-container">
-        <ContainerUsuario recarregarTodasImgs={recarregarTodasImgs}/>
-        {props.children}
+        <ContainerUsuario recarregarTodasImgs={recarregarTodasImgs} />
+        {carregarPagina ? props.children : <Loading />}
       </Box>
     </Box>
   );
